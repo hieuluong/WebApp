4a25922fae746302f74649e3da341a0761211cc4
hieuluong <hieu@archway.co.jp>
2018-10-04 12:44:43 +0900

    add project

---
+++ src/IngressSample/Controllers/Summary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IngressSample.Controllers
+{
+    public class Summary
+    {
+        public int Total(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ test/IngressSample.Test/SummaryTest.cs
@@ -0,0 +1,18 @@
+﻿using IngressSample.Controllers;
+using System;
+using Xunit;
+
+namespace IngressSample.Test
+{
+    public class SummaryTest
+    {
+        [Fact]
+        public void TotalTest()
+        {
+            var summary = new Summary();
+            var result = summary.Total(3, 5);
+
+            Assert.Equal(8, result);
+        }
+    }
+}
